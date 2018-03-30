7d5cbdf38423c08424d6f3bb005e8a6781ac61e1
Jatin Agrawal <jatin86400@gmail.com>
2018-03-30 19:24:49 +0530

    added a 3d vector for timetable and various other changes

--- timetableoptimisation/genetic_project.cpp
+++ timetableoptimisation/genetic_project.cpp
@@ -7,22 +7,79 @@ public:
     string department_name;//Name of department
     int totalsubs;//count of total no of subject of that department.
     vector<int> subs;// A vector is just a array. This array stores the index of subjects that are in cariculum of this department.
-    vector<int> timetable[5];//This is a 2D array initialization for timetable of that department.
+    vector<vector<vector<int> > > timetable;//It is a 3d vector.
+    vector<int> sub_imp;//I took a limit that total subs cannot be more that 100;
+    vector<int> count_sub_occur;
     department()
     {
-        for(int i=0;i<5;i++)
-        {
-            timetable[i].assign(9,0);
-        }
+        sub_imp.assign(100,0);
+        timetable.assign(4,vector<vector<int> >(5, vector<int>(9)));//It signifies 4 timetable for each dep, with 5 rows and 9 cols.Each timetable is a member of population.
     }
     void initialize_para()//Here we initialize the parameters to start our genetic algorithm.
     {
-
+        for(int k=0;k<4;k++)
+        {
+            count_sub_occur.assign(100,0)
+            for(int i=0;i<4;i++)
+            {
+                for(int j=0;j<5;j++)
+                {
+                    for(int k=0;k<9;k++)
+                    {
+                        count_sub_occur[timetable[i][j][k]]++;
+                    }
+                }
+            }
+        while(check_condition())
+        {
+            for(int i=0;i<5;i++)
+            {
+                for(int j=0;j<9;j++)
+                {
+                    if(timetable[k][i][j]==0)
+                    {
+                        int temp = give_random_sub();
+                        timetable[k][i][j] = subs[temp];
+                        count_sub_occur[temp]++;
+                    }
+                }
+            }
+        }
+        }
+    }
+    bool check_condition()//It checks condition, the condition is that all the subs of that dep should have their share of 4 hours.
+    {
+        for(int i=0;i<totalsubs;i++)
+        {
+            if(count_sub_occur[subs[i]]<4)
+                return true;
+        }
+            return false;
+    }
+    int give_random_sub()//It checks whether the sub that is generated randomly has completed its 4 hr quota or not, if completed than it gives another random no.
+    {
+        int temp = rand()%(totalsubs);
+        while(count_sub_occur[subs[temp]]>=4)
+        {
+            temp = rand()%(totalsubs);
+        }
+        return temp;
+    }
+    void set_sub_imp(string sub[])//Give importance of each subject for each department
+    {
+        for(int i=0;i<totalsubs;i++)
+        {
+            int x;
+            cout<<"What is importance of "<<sub[subs[i]]<<" for "<<department_name<<" department in scale of 1 to 10 (1 signifies very less important and 10 signifies extremely important."<<endl;
+            cin>>x;
+            sub_imp[subs[i]] = x;
+        }
     }
 };
 
 int main()
 {
+    srand(time(0));
     int d,n;//d is for number of departments and n is for total number of subjects.
     cout<<"Please define the number of departments :"<<endl;
     cin>>d;
@@ -50,9 +107,23 @@ int main()
             int temp;
             cin>>temp;
             sub_array[i].push_back(temp-1);//store the department index to which the subject is common.
-            departments[temp-1].subs.push_back(i+1);//For department no 'temp-1' we know that this subject is in its cariculum so we add this subject in subject list of that department.
+            departments[temp-1].subs.push_back(i);//For department no 'temp-1' we know that this subject is in its cariculum so we add this subject in subject list of that department.
             departments[temp-1].totalsubs++;//we update total subjects count for that department.
         }
+    }
+    /*for(int i=0;i<4;i++)
+    {
+        for(int j=0;j<5;j++)
+        {
+            for(int k=0;k<9;k++)
+            {
+                cout<<departments[0].timetable[i][j][k]<<endl;
+            }
+        }
+    }*/
+    for(int i=0;i<d;i++)
+    {
+        departments[i].set_sub_imp(sub);
     }
 
 }
