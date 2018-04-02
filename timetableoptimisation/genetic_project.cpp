b77cdaece0d4c32b71d17e7a337a43b36919d601
Jatin Agrawal <jatin86400@gmail.com>
2018-04-02 16:29:28 +0530

    Added function for crossover and mutaution

--- timetableoptimisation/genetic_project.cpp
+++ timetableoptimisation/genetic_project.cpp
@@ -8,6 +8,7 @@ public:
     int totalsubs;//count of total no of subject of that department.
     vector<int> subs;// A vector is just a array. This array stores the index of subjects that are in cariculum of this department.
     vector<vector<vector<int> > > timetable;//It is a 3d vector.
+    vector<vector<vector<int> > > buffer;
     vector<int> sub_imp;//I took a limit that total subs cannot be more that 100;
     vector<int> count_sub_occur;
     department()
@@ -15,33 +16,49 @@ public:
         totalsubs = 0;
         sub_imp.assign(100,0);
         count_sub_occur.assign(100,0);
+        buffer.assign(4,vector<vector<int> >(5, vector<int>(9,-1)));
         timetable.assign(4,vector<vector<int> >(5, vector<int>(9,-1)));//It signifies 4 timetable for each dep, with 5 rows and 9 cols.Each timetable is a member of population.-1 represents no subject.
+    }
+    void initialize_count_sub_occur(int key,int index)
+    {
+        count_sub_occur.assign(100,0);
+        for(int i=0;i<5;i++)
+            {
+                for(int j=0;j<9;j++)
+                {
+                        //cout<<"inside for"<<endl;
+                        if(key==1)
+                        {
+                            if(timetable[index][i][j]!=-1)
+                            {
+                            //cout<<"inside if"<<endl;
+                                count_sub_occur[timetable[index][i][j]]++;
+                            }
+                        }
+                        else
+                        {
+                            if(buffer[index][i][j]!=-1)
+                            {
+                            //cout<<"inside if"<<endl;
+                                count_sub_occur[buffer[index][i][j]]++;
+                            }
+                        }
+                }
+            }
     }
     void initialize_para(vector<vector<int> > &sub_array,vector<department> &departments)//Here we initialize the parameters to start our genetic algorithm.
     {
        // cout<<"Inside initialize function"<<endl;
         for(int k=0;k<4;k++)
         {
-            count_sub_occur.assign(100,0);
-            for(int i=0;i<5;i++)
-            {
-                for(int j=0;j<9;j++)
-                {
-                        //cout<<"inside for"<<endl;
-                        if(timetable[k][i][j]!=-1)
-                        {
-                            //cout<<"inside if"<<endl;
-                        count_sub_occur[timetable[k][i][j]]++;
-                        }
-                }
-            }
+            initialize_count_sub_occur(1,k);
         while(check_condition())
         {
             for(int i=0;i<5;i++)
             {
                 for(int j=0;j<9;j++)
                 {
-                    if(timetable[k][i][j]==-1 && rand()%2==1)//this condition is true when the block in which we are in is not filled yet and also 50% chance of selecting the block.
+                    if(timetable[k][i][j]==-1 && rand()%3==1)//this condition is true when the block in which we are in is not filled yet and also 33% chance of selecting the block.
                     {
                         int temp = give_random_sub(sub_array,departments,k,i,j);
                         timetable[k][i][j] = subs[temp];
@@ -70,7 +87,7 @@ public:
         //cout<<"inside check condition"<<endl;
         for(int i=0;i<totalsubs;i++)
         {
-            if(count_sub_occur[subs[i]]<4)
+            if(count_sub_occur[subs[i]]!=4)
                 return true;
         }
             return false;
@@ -117,7 +134,160 @@ public:
         }
     }
 };
+void apply_condition(vector<department> &departments,int d, vector<vector<int> > &sub_array)
+{
+    cout<<"inside apply condition"<<endl;
+     for(int i=0;i<4;i++)
+    {
+        for(int j=0;j<d;j++)
+        {
+            departments[j].initialize_count_sub_occur(0,i);
+            while(departments[j].check_condition())
+            {
+                //cout<<"inside apply condition while"<<endl;
+                for(int k=0;k<departments[j].totalsubs;k++)
+                {
+                    int subject = departments[j].subs[k];
+                    cout<<"subject: "<<subject<<"count :"<<departments[j].count_sub_occur[subject]<<endl;
 
+                    if(departments[j].count_sub_occur[subject]<4)
+                    {
+                        cout<<"inside first if"<<endl;
+                        for(int l=0;l<5;l++)
+                        {
+                            int count = 0;
+
+                            for(int r=0;r<9;r++)
+                            {
+                                bool key = false;
+                                for(int x=0;x<sub_array[subject].size();x++)
+                                {
+                                    if(departments[sub_array[subject][x]].buffer[i][l][r]!=-1)
+                                        key=true;
+                                }
+                                if(!key)
+                                {
+                                    cout<<"inside decider if"<<endl;
+                                    for(int x=0;x<sub_array[subject].size();x++)
+                                    {
+                                        departments[sub_array[subject][x]].buffer[i][l][r] = subject;
+                                        departments[sub_array[subject][x]].count_sub_occur[subject]++;
+                                    }
+                                    count++;
+                                    break;
+
+                                }
+
+                            }
+                            if(count>0)
+                                break;
+                        }
+                        departments[j].initialize_count_sub_occur(0,i);
+                    }
+                    else
+                    {
+                        if(departments[j].count_sub_occur[subject]>4)
+                        {
+                            for(int l=0;l<5 ;l++)
+                            {
+                                bool key = false;
+                                for(int r=0;r<9;r++)
+                                {
+                                    if(departments[j].buffer[i][l][r]==subject)
+                                    {
+                                        key = true;
+                                        cout<<"inside less decider if"<<endl;
+                                        for(int x=0;x<sub_array[subject].size();x++)
+                                        {
+                                            departments[sub_array[subject][x]].buffer[i][l][r] = -1;
+                                            departments[sub_array[subject][x]].count_sub_occur[subject]--;
+                                        }
+                                        break;
+                                    }
+                                }
+                                if(key)
+                                    break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+void crossover(vector<department> &departments,int d)
+{
+    cout<<"inside crossover"<<endl;
+    for(int i=0;i<4;i++)
+    {
+        int temp = rand()%4;
+        while(temp==i)
+        {
+            cout<<"inside while"<<endl;
+            temp=rand()%4;
+        }
+        for(int j=0;j<9;j++)
+        {
+            if(rand()%3==1)
+            {
+                for(int k=0;k<5;k++)
+                {
+                     for(int l=0;l<d;l++)
+                    {
+                        departments[l].buffer[i][k][j] = departments[l].timetable[temp][k][j];
+                    }
+                }
+            }
+            else
+            {
+                 for(int k=0;k<5;k++)
+                {
+                     for(int l=0;l<d;l++)
+                    {
+                        departments[l].buffer[i][k][j] = departments[l].timetable[i][k][j];
+                    }
+                }
+            }
+        }
+    }
+    cout<<"outside crossover"<<endl;
+}
+void mutation(vector<department> &departments,int d,vector<vector<int> > &sub_array)
+{
+    for(int i=0;i<4;i++)
+    {
+        for(int j=0;j<d;j++)
+        {
+            int temp = rand()%10;
+            for(int r = 0;r<temp;r++)
+            {
+                int subject = rand()%departments[j].totalsubs;
+                subject = departments[j].subs[subject];
+                for(int k=0;k<5;k++)
+                {
+                    for(int l=0;l<9;l++)
+                    {
+                        bool key = false;
+                        for(int x=0;x<sub_array[subject].size();x++)
+                        {
+                            if(departments[sub_array[subject][x]].timetable[i][k][l]!= -1)
+                                key = true;
+                        }
+                        if(!key && rand()%5==1)
+                        {
+                            for(int x=0;x<sub_array[subject].size();x++)
+                            {
+                                departments[sub_array[subject][x]].timetable[i][k][l]= subject;
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
+
+    }
+}
 int main()
 {
     srand(time(0));
@@ -129,7 +299,7 @@ int main()
     for(int i=0;i<d;i++)
     {
         scanf("\n");//Please ignore this if you din't get it.
-        getline(cin,departments[i].department_name);//here we read the whole line, hence getline is used instead of cin.
+        getline(cin,departments[i].department_name);//here we read the whole line, hence getline() is used instead of cin.
     }
     cout<<"Please tell the total no of subjects: "<<endl;
     cin>>n;
@@ -161,6 +331,10 @@ int main()
     {
         departments[i].set_sub_imp(sub);
     }*/
+    crossover(departments,d);
+    apply_condition(departments,d,sub_array);
+    mutation(departments,d,sub_array);
+    apply_condition(departments,d,sub_array);
     for(int l=0;l<d;l++)
     {
         cout<<"Department: "<<departments[l].department_name<<endl;
@@ -170,7 +344,7 @@ int main()
             {
                 for(int k=0;k<9;k++)
                 {
-                    cout<<departments[l].timetable[i][j][k]<<" ";
+                    cout<<departments[l].buffer[i][j][k]<<" ";
                 }
                 cout<<endl;
             }
